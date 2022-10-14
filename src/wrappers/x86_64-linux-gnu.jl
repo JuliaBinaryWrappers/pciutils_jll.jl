c570c94c5cebbbab35331eb8c02e6b4e74667dea
jlbuild <juliabuildbot@gmail.com>
2022-10-14 19:38:55 +0000

    pciutils_jll build 3.7.0+1

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -9,7 +9,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libpci,
-        "lib/libpci.so.3",
+        "lib/libpci.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
